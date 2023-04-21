cbcea6e0f5159e338dfcdf9f9747ca8c561a386d
horodeski <geovana.horodeski06@gmail.com>
2023-04-20 23:18:03 -0300

    começando a main e o aside

---
+++ FrontEnd/src/components/AsideComp.vue
@@ -0,0 +1,45 @@
+<script>
+export default {}
+</script>
+<template>
+  <aside>
+    <div class="perfil">
+      <img src="src/assets/img/edudasilva.jpeg" alt="" />
+      <div class="infos">
+        <div class="nome-user">
+            <h3>Eduardo da Silva</h3>
+            <h3>@edudasilva</h3>
+        </div>
+        <box-icon size="1em" color="#fff" class="icon" name="bell"></box-icon>
+      </div>
+    </div>
+    <div class="contatos">
+      <div>
+        <h2>Contatos</h2>
+        <box-icon size="2em" name="chevron-right"></box-icon>
+      </div>
+      <div class="perfil-contatos">
+        <img src="src/assets/img/edudasilva.jpeg" alt="" />
+        <div>
+          <h3>@isahumanas</h3>
+        </div>
+        <box-icon size="2em" type="solid" name="phone"></box-icon>
+      </div>
+      <div class="perfil-contatos">
+        <img src="src/assets/img/edudasilva.jpeg" alt="" />
+        <div>
+          <h3>@isahumanas</h3>
+        </div>
+        <box-icon size="2em" type="solid" name="phone"></box-icon>
+      </div>
+      <div class="perfil-contatos">
+        <img src="src/assets/img/edudasilva.jpeg" alt="" />
+        <div>
+          <h3>@isahumanas</h3>
+        </div>
+        <box-icon size="2em" type="solid" name="phone"></box-icon>
+      </div>
+    </div>
+    <div></div>
+  </aside>
+</template>
--- FrontEnd/src/views/HomeView.vue
+++ FrontEnd/src/views/HomeView.vue
@@ -1,7 +1,48 @@
 <script>
-export default {};
+import 'boxicons'
+import AsideComp from '../components/AsideComp.vue'
+export default {
+  components: { AsideComp },}
 </script>
 
 <template>
-  a
+  <main>
+    <div class="pesquisa">
+      <div>
+        <box-icon name="search-alt-2"></box-icon>
+        <input placeholder="O que você procura?" type="text" />
+      </div>
+    </div>
+    <section class="valores">
+      <div class="card-valores">
+        <box-icon color="#fff" name="line-chart-down"></box-icon>
+        <div>
+          <h2>00</h2>
+          <h3>vendas</h3>
+        </div>
+      </div>
+      <div class="card-valores">
+        <box-icon color="#fff" name="line-chart-down"></box-icon>
+        <div>
+          <h2>00</h2>
+          <h3>vendas</h3>
+        </div>
+      </div>
+      <div class="card-valores">
+        <box-icon color="#fff" name="line-chart-down"></box-icon>
+        <div>
+          <h2>00</h2>
+          <h3>vendas</h3>
+        </div>
+      </div>
+      <div class="card-valores">
+        <box-icon color="#fff" name="line-chart-down"></box-icon>
+        <div>
+          <h2>00</h2>
+          <h3>vendas</h3>
+        </div>
+      </div>
+    </section>
+  </main>
+  <AsideComp />
 </template>
