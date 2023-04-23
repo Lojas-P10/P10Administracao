4f3cde8ee89a08d2d47697109841305f00307ea5
horodeski <geovana.horodeski06@gmail.com>
2023-04-23 12:47:28 -0300

    graficos

--- FrontEnd/src/views/HomeView.vue
+++ FrontEnd/src/views/HomeView.vue
@@ -2,7 +2,8 @@
 import 'boxicons'
 import AsideComp from '../components/AsideComp.vue'
 export default {
-  components: { AsideComp },}
+  components: { AsideComp }
+}
 </script>
 
 <template>
@@ -13,29 +14,42 @@ export default {
         <input placeholder="O que você procura?" type="text" />
       </div>
     </div>
-    <section class="valores">
-      <div class="card-valores">
+    <section class="vendas">
+      <div class="card-vendas">
         <box-icon class="icon" size="2.5em" color="#fff" name="line-chart-down"></box-icon>
         <div>
           <h2>00</h2>
           <h3>Volume de vendas</h3>
         </div>
       </div>
-      <div class="card-valores">
+      <div class="card-vendas">
         <box-icon class="icon" size="2.5em" color="#fff" name="line-chart-down"></box-icon>
         <div>
           <h2>00</h2>
           <h3>Ticket médio</h3>
         </div>
       </div>
-      <div class="card-valores">
+      <div class="card-vendas">
         <box-icon class="icon" size="2.5em" color="#fff" name="line-chart-down"></box-icon>
         <div>
           <h2>00</h2>
           <h3>Margem de lucro</h3>
         </div>
       </div>
-
+    </section>
+    <section class="estoque">
+      <div class="card-estoque">
+        <h2>Unidades vendidas</h2>
+        <h1>25</h1>
+      </div>
+      <div class="card-estoque">
+        <h2>Estoque mínimo</h2>
+        <h1>38</h1>
+      </div>
+      <div class="card-estoque">
+        <h2>Produtos em estoque</h2>
+        <h1>71</h1>
+      </div>
     </section>
   </main>
   <AsideComp />
