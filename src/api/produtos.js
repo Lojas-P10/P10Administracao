30d69dfaffdce3756e379a815b07f28bf313c10e
horodeski <geovana.horodeski06@gmail.com>
2023-08-13 00:12:57 -0300

    Criação e implementação da API de produtos

---
+++ src/api/produtos.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+export default class ProdutosApi {
+  async buscarTodosOsProdutos() {
+    const { data } = await axios.get('http://127.0.0.1:8000/api/produtos/');
+    return data;
+  }
+  async adicionarProduto(produto) {
+    const { data } = await axios.post('http://127.0.0.1:8000/api/produtos/', produto);
+    return data;
+  }
+  async atualizarProduto(produto) {
+    const { data } = await axios.put(`http://127.0.0.1:8000/api/produtos/${produto.id}/`, produto);
+    return data;
+  }
+  async excluirProduto(id) {
+    const { data } = await axios.delete(`http://127.0.0.1:8000/api/produtos/${id}/`);
+    return data;
+  }
+}
---
+++ src/views/ProdutosView.vue
@@ -0,0 +1,104 @@
+<script setup>
+import ProdutosApi from "@/api/produtos";
+import {ref, onMounted} from 'vue';
+const produtosApi = new ProdutosApi();
+
+const produtos = ref([]);
+
+onMounted(async() => {
+  produtos.value = await produtosApi.buscarTodosOsProdutos()
+})
+</script>
+
+<template>
+  <div>
+    <FiltroComp />
+    <table>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Produto</th>
+          <th>Categoria</th>
+          <th>Fornecedor</th>
+          <th>Quantidade</th>
+          <th>preco</th>
+          <th>Promoção</th>
+          <th>Desconto</th>
+          <th>    <button class="add">Adicionar produto</button></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="produto in produtos" :key="produto.id">
+          <td>{{ produto.id }}</td>
+          <td><a href="#">{{ produto.nome }}</a></td>
+          <td>{{ produto.categoria }}</td>
+          <td>{{ produto.fornecedor }}</td>
+          <td>{{ produto.quantidade }}</td>
+          <td>{{ produto.preco }}</td>
+          <td>{{ produto.promocao }}</td>
+          <td>{{ produto.desconto }}</td>
+          <td>
+            <button ><box-icon name='edit'></box-icon></button>
+            <button><box-icon name='trash' ></box-icon></button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+  <!--         <div class="senao" v-else>
+          <span class="aviso">Não existem editoras cadastradas</span>
+          <i class="bx bx-error"></i>
+        </div> -->
+</template>
+
+<style scoped>
+div {
+  margin: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+}
+table {
+  width: -webkit-fill-available;
+  border-radius: 0px;
+  border-collapse: collapse;
+  margin-top: 10px;
+}
+thead {
+  background-color: #eee;
+}
+tbody {
+  background-color: #f3f3f3;
+}
+table tr td {
+  padding: 10px;
+  text-align: center;
+
+}
+td button {
+  	text-align: center;
+	width: 50px;
+	padding: 5px 0;
+  margin-right: 10px;
+	border-radius: 10px;
+
+}
+th {
+  font-weight: bold;
+  padding: 10px;
+}
+.add {
+  background: #00bf63;
+  color: white;
+  padding: 5px;
+  border: 2px solid #018042;
+  border-radius: 5px;
+  transition: background 0.2s;
+  width: 200px;
+}
+.add:hover {
+  cursor: pointer;
+  background: #ffffff;
+  color: #018042;
+}
+</style>
