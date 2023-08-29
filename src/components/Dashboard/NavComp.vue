bddb49a59e94d3db2923692a7f007f1b80b83940
horodeski <geovana.horodeski06@gmail.com>
2023-08-29 15:22:37 -0300

    Criação da navegação no dashboard

---
+++ src/components/Dashboard/NavComp.vue
@@ -0,0 +1,49 @@
+<script setup>
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+
+const isRouteActive = (...paths) => {
+  return paths.some(path => route.path.includes(path));
+};
+</script>
+
+<template>
+  <nav>
+    <div class="navegacao">
+      <div class="links">
+        <RouterLink to="/dashboard/produtos" :class="{ active: isRouteActive('/dashboard/produtos')}"> Produtos </RouterLink>
+        <RouterLink to="/dashboard/parceiros" :class="{ active: isRouteActive('/dashboard/parceiros') }"> Parceiros </RouterLink>
+        <RouterLink to="/" > Vendas </RouterLink>
+      </div>
+    </div>
+  </nav>
+</template>
+
+
+<style scoped>
+nav {
+  position : absolute;
+  top: 4%;
+}
+.links {
+  display: flex;
+  gap: 3em;
+  color: var(--c-text-tertiary);
+  border-bottom: 2px solid var(--c-gray-600);
+  align-items: center;
+}
+.links a {
+  padding-top: 1rem;
+  padding-bottom: 1rem;
+  text-decoration: none;
+  border-bottom: 2px solid transparent;
+  display: inline-flex;
+  transition: 0.25s ease;
+}
+.links a.active,
+.links a:hover,
+.links a:focus {
+  color: var(--c-text-primary);
+  border-color: var(--c-text-primary);
+}</style>
