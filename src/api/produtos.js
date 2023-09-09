77816c05d2c125bdf2c9504bd3d798c85884314a
horodeski <geovana.horodeski06@gmail.com>
2023-09-08 22:07:54 -0300

    Alteração nos endpoints da API

--- src/api/produtos.js
+++ src/api/produtos.js
@@ -1,19 +1,19 @@
 import axios from 'axios';
 export default class ProdutosApi {
   async buscarTodosOsProdutos() {
-    const { data } = await axios.get('https://p10backend-eugreg-dev.fl0.io/api/produtos/1/');
+    const { data } = await axios.get('https://p10backend-eugreg-dev.fl0.io/api/produtos/');
     return data;
   }
   async adicionarProduto(produto) {
-    const { data } = await axios.post('http://127.0.0.1:8000/api/produtos/', produto);
+    const { data } = await axios.post('https://p10backend-eugreg-dev.fl0.io/api/produtos/', produto);
     return data;
   }
   async atualizarProduto(produto) {
-    const { data } = await axios.put(`http://127.0.0.1:8000/api/produtos/${produto.id}/`, produto);
+    const { data } = await axios.put(`https://p10backend-eugreg-dev.fl0.io/api/produtos/${produto.id}/`, produto);
     return data;
   }
   async excluirProduto(id) {
-    const { data } = await axios.delete(`http://127.0.0.1:8000/api/produtos/${id}/`);
+    const { data } = await axios.delete(`https://p10backend-eugreg-dev.fl0.io/api/produtos/${id}/`);
     return data;
   }
 }
