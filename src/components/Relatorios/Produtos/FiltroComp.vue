65036dc473a53ae6b598d33d4cbd1315f8a0ac29
horodeski <geovana.horodeski06@gmail.com>
2023-09-08 22:11:19 -0300

    Desenvolvimento do filtro de produtos

--- src/components/Relatorios/Produtos/FiltroComp.vue
+++ src/components/Relatorios/Produtos/FiltroComp.vue
@@ -1,176 +1,174 @@
-<script setup>
-
-</script>
+<script setup></script>
 <template>
-    <h2>Filtrar</h2>
-    <form class="filter">
-        <div>
-            <label>Filtrar por produto</label>
-            <input type="" />
-        </div>
-        <!--Data inicio e fim-->
-        <div class="dropdown">
-            <label for="">Dias da semana</label>
-            <select class="dropdown-select">
-                <option value="">Dias da semana</option>
-                <option value="1">Option #1</option>
-                <option value="2">Option #2</option>
-                <option value="3">Option #3</option>
-            </select>
-        </div>
-        <div class="dropdown">
-            <select class="dropdown-select">
-                <option value="">Couleur</option>
-                <option value="1">Option #1</option>
-                <option value="2">Option #2</option>
-                <option value="3">Option #3</option>
-            </select>
-        </div>
-        <div class="dropdown">
-            <select class="dropdown-select">
-                <option value="">Forme</option>
-                <option value="1">Option #1</option>
-                <option value="2">Option #2</option>
-                <option value="3">Option #3</option>
-            </select>
-        </div>
-        <div class="dropdown">
-            <select class="dropdown-select">
-                <option value="">Matière</option>
-                <option value="1">Option #1</option>
-                <option value="2">Option #2</option>
-                <option value="3">Option #3</option>
-            </select>
-        </div>
-        <div class="sub-main">
-            <button class="button-two"><span>Buscar</span></button>
-        </div>
-
-    </form>
+  <form class="filter">
+    <div>
+      <label>Filtrar por produto</label>
+      <input placeholder="batata doce..." type="" />
+    </div>
+    <div class="container">
+      <div class="dropdown">
+        <label for="">Fornecedor</label>
+        <select class="dropdown-select">
+          <option value="">Dias da semana</option>
+          <option value="1">Option #1</option>
+          <option value="2">Option #2</option>
+          <option value="3">Option #3</option>
+        </select>
+      </div>
+      <div class="dropdown">
+        <label for="">Categoria</label>
+        <select class="dropdown-select">
+          <option value="">Dias da semana</option>
+          <option value="1">Option #1</option>
+          <option value="2">Option #2</option>
+          <option value="3">Option #3</option>
+        </select>
+      </div>
+    </div>
+    <div class="container">
+      <div class="dropdown">
+        <label for="">Sazonal</label>
+        <select class="dropdown-select">
+          <option value="">Dias da semana</option>
+          <option value="1">Option #1</option>
+          <option value="2">Option #2</option>
+          <option value="3">Option #3</option>
+        </select>
+      </div>
+      <div class="dropdown">
+        <label for="">Desconto</label>
+        <select class="dropdown-select">
+          <option value="">Dias da semana</option>
+          <option value="1">Option #1</option>
+          <option value="2">Option #2</option>
+          <option value="3">Option #3</option>
+        </select>
+      </div>
+    </div>
+    <div class="dropdown">
+      <label for="">Preço</label>
+      <input type="text">
+    </div>
+    <div class="sub-main">
+      <button class="button-two"><span>Buscar</span></button>
+    </div>
+  </form>
 </template>
-
 
 <style scoped>
 .filter {
-    display: flex;
-    align-items: center;
-    flex-direction: column;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
 }
-
-.dropdown {
-    display: inline-block;
-    position: relative;
-    overflow: hidden;
-    height: 28px;
-    width: 100%;
-    background: #f2f2f2;
-    border: 1px solid;
-    border-color: white #f7f7f7 whitesmoke;
-    border-radius: 3px;
+form {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+}
+form div {
+  display: flex;
+  gap: 5px;
+  flex-direction: column;
+  width: 100%;
+}
+.container {
+  display: flex;
+  flex-direction: row;
+  gap: 10px;
 }
 
 .dropdown:before,
 .dropdown:after {
-    content: '';
-    position: absolute;
-    z-index: 2;
-    top: 9px;
-    right: 10px;
-    width: 0;
-    height: 0;
-    border: 4px dashed;
-    border-color: #888888 transparent;
-    pointer-events: none;
+  content: '';
+  position: absolute;
+  z-index: 2;
+  top: 9px;
+  right: 10px;
+  width: 0;
+  height: 0;
+  border: 4px dashed;
+  pointer-events: none;
 }
-
+input,
+.dropdown-select {
+  background-color: #333;
+  border-radius: 10px;
+  height: 2.8em;
+}
 .dropdown:before {
-    border-bottom-style: solid;
-    border-top: none;
+  border-bottom-style: solid;
+  border-top: none;
 }
 
 .dropdown:after {
-    margin-top: 7px;
-    border-top-style: solid;
-    border-bottom: none;
+  margin-top: 7px;
+  border-top-style: solid;
+  border-bottom: none;
 }
 
 .dropdown-select {
-    position: relative;
-    width: 100%;
-    margin: 0;
-    padding: 6px 8px 6px 10px;
-    height: 28px;
-    line-height: 14px;
-    font-size: 12px;
-    color: #62717a;
-    text-shadow: 0 1px white;
-    background: #f2f2f2;
-    /* Fallback for IE 8 */
-    background: rgba(0, 0, 0, 0) !important;
-    /* "transparent" doesn't work with Opera */
-    border: 0;
-    border-radius: 0;
-    -webkit-appearance: none;
+  position: relative;
+  width: 100%;
+  margin: 0;
+  padding: 6px 8px 6px 10px;
+  color: #f5f5f5;
+  border: 0;
+  -webkit-appearance: none;
 }
 
 .dropdown-select:focus {
-    z-index: 3;
-    width: 100%;
-    color: #394349;
-    outline: 2px solid #49aff2;
-    outline: 2px solid -webkit-focus-ring-color;
-    outline-offset: -2px;
+  z-index: 3;
+  width: 100%;
+  color: #111;
+  outline: 2px solid #49aff2;
+  outline: 2px solid -webkit-focus-ring-color;
+  outline-offset: -2px;
 }
 
-.dropdown-select>option {
-    margin: 3px;
-    padding: 6px 8px;
-    text-shadow: none;
-    background: #f2f2f2;
-    border-radius: 3px;
-    cursor: pointer;
-}
-
-.sub-main {}
-
-.button-two {
-    text-align: center;
-    cursor: pointer;
-    font-size: 14px;
-    margin: 0 0 0 20px;
-    color: white;
+.dropdown-select > option {
+  margin: 3px;
+  padding: 6px 8px;
+  text-shadow: none;
+  background: #fff;
+  border-radius: 3px;
+  cursor: pointer;
 }
 
 .button-two {
-    border-radius: 2px;
-    background-color: #00bf63;
-    border: none;
-    padding: 5px;
-    transition: all 0.5s;
+  text-align: center;
+  cursor: pointer;
+  border-radius: 10px;
+  background-color: #00bf63;
+  padding: 5px;
+  color: white;
+  border: none;
+  transition: all 0.5s;
+  margin-top: 5px;
 }
 
 .button-two span {
-    cursor: pointer;
-    display: inline-block;
-    position: relative;
-    transition: 0.5s;
+  cursor: pointer;
+  display: inline-block;
+  position: relative;
+  transition: 0.5s;
 }
 
 .button-two span:after {
-    content: '»';
-    position: absolute;
-    opacity: 0;
-    top: 0;
-    right: -20px;
-    transition: 0.5s;
+  content: '»';
+  position: absolute;
+  opacity: 0;
+  top: 0;
+  right: -20px;
+  transition: 0.5s;
 }
 
 .button-two:hover span {
-    padding-right: 25px;
+  padding-right: 25px;
 }
 
 .button-two:hover span:after {
-    opacity: 1;
-    right: 0;
+  opacity: 1;
+  right: 0;
 }
 </style>
--- src/views/Relatorios/ProdutosView.vue
+++ src/views/Relatorios/ProdutosView.vue
@@ -1,16 +1,23 @@
 <script setup>
-import FiltroComp from "../../components/Relatorios/Produtos/FiltroComp.vue";
-import TabelaComp from "../../components/Relatorios/Produtos/TabelaComp.vue";
-
+import FiltroComp from '../../components/Relatorios/Produtos/FiltroComp.vue'
+import TabelaComp from '../../components/Relatorios/Produtos/TabelaComp.vue'
 </script>
 <template>
-  <main class="app-body-main-content">
+  <main>
+    <header>
+      <h2>Produtos</h2>
+    </header>
+    <FiltroComp />
     <TabelaComp />
   </main>
-  <aside class="app-body-sidebar">
-    <FiltroComp />
-  </aside>
 </template>
 
-
-<style scoped ></style>
+<style scoped>
+header {
+  display: flex;
+  align-items: center;
+  width: 100%;
+  padding-bottom: 0.75rem;
+  border-bottom: 1px solid var(--c-gray-600);
+}
+</style>
