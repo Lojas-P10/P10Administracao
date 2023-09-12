d2d42839d2ca1850f77b383c8eb43b2f0b4accac
horodeski <geovana.horodeski06@gmail.com>
2023-09-12 09:14:01 -0300

    Desenvolvendo o relatório de produtos

--- src/views/Relatorios/ProdutosView.vue
+++ src/views/Relatorios/ProdutosView.vue
@@ -7,8 +7,10 @@ import TabelaComp from '../../components/Relatorios/Produtos/TabelaComp.vue'
     <header>
       <h2>Produtos</h2>
     </header>
-    <FiltroComp />
-    <TabelaComp />
+    <section>
+      <FiltroComp />
+      <TabelaComp />
+    </section>
   </main>
 </template>
 
@@ -16,8 +18,19 @@ import TabelaComp from '../../components/Relatorios/Produtos/TabelaComp.vue'
 header {
   display: flex;
   align-items: center;
+  margin-bottom: 2em;
   width: 100%;
   padding-bottom: 0.75rem;
   border-bottom: 1px solid var(--c-gray-600);
+}
+main {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+}
+header h2 {
+  margin: 0
+}
+section {
 }
 </style>
