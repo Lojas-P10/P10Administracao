d1f7ca254378b4629032cda8652c6907a288c0c7
horodeski <geovana.horodeski06@gmail.com>
2023-10-07 22:22:52 -0300

    Alterações na api de imagens

--- src/api/marcas.js
+++ src/api/marcas.js
@@ -1,19 +1,19 @@
 import axios from 'axios';
 export default class MarcaApi {
   async buscarTodasAsMarcas() {
-    const { data } = await axios.get('http://p10backend-eugreg-dev.fl0.io/api/marca/');
+    const { data } = await axios.get('https://p10backend-eugreg-dev.fl0.io/api/marca/');
     return data;
   }
   async adicionarMarca(marca) {
-    const { data } = await axios.post('http://p10backend-eugreg-dev.fl0.io/api/marca/', marca);
+    const { data } = await axios.post('https://p10backend-eugreg-dev.fl0.io/api/marca/', marca);
     return data;
   }
   async atualizarMarca(marca) {
-    const { data } = await axios.put(`http://p10backend-eugreg-dev.fl0.io/api/marca/${marca.id}/`, marca);
+    const { data } = await axios.put(`https://p10backend-eugreg-dev.fl0.io/api/marca/${marca.id}/`, marca);
     return data;
   }
   async excluirMarca(id) {
-    const { data } = await axios.delete(`http://p10backend-eugreg-dev.fl0.io/api/marca/${id}/`);
+    const { data } = await axios.delete(`https://p10backend-eugreg-dev.fl0.io/api/marca/${id}/`);
     return data;
   }
 }
