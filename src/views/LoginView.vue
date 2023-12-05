65ef52552f8aee66e232786c66a167c8600532cd
horodeski <geovana.horodeski06@gmail.com>
2023-12-05 10:57:28 -0300

    Alteração no login

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -2,7 +2,7 @@
 import { reactive } from 'vue'
 
 import { useAuthStore } from '@/stores/auth'
-import { useRouter} from 'vue-router'
+import { useRouter } from 'vue-router'
 
 const authStore = useAuthStore()
 const router = useRouter()
@@ -16,35 +16,47 @@ async function submit() {
   try {
     await authStore.login(user)
     router.push('/dashboard/produtos/')
-  }catch (error) {
-      console.error(error);
-    }
-  
+  } catch (error) {
+    console.error(error);
+  }
+
 } 
 </script>
 
 <template>
   <main>
-      <div class="login-box">
-        <div class="hello-world">
-          <img src="../../public/logo/LojasP10/logo-menor.png" alt="" />
-        </div>
-        <form @submit.prevent="submit">
-          <h1>Entre na sua conta</h1>
-          <div class="user-box">
-            <div><box-icon color="var(--c-white)" name="envelope"></box-icon></div>
-            <input required="" v-model="user.email" placeholder="email" type="email" />
-          </div>
-          <div class="user-box">
-            <div><box-icon color="var(--c-white)" name="lock-alt"></box-icon></div>
-            <input required="" v-model="user.password" placeholder="senha" type="password" />
-          </div>
-          <button @submit.prevent="submit" type="submit" class="btn-blue">
-            entrar 
-          </button>
-        </form>
+  <div class="login-box">
+      <div class="hello-world">
+        <img src="../../public/logo/LojasP10/logo-menor.png" alt="" />
       </div>
-    
+      <form @submit.prevent="submit">
+        <h1>Entre na sua conta</h1>
+        <div class="user-box">
+          <div>
+            <input id="sem-efeito" required="" v-model="user.email" placeholder="email" type="email" />
+            <div class="input-back">
+              <div class="button"><box-icon color="var(--c-white)" name="envelope"></box-icon></div>
+            </div>
+          </div>
+          <div>
+            <input id="sem-efeito" required="" v-model="user.password" placeholder="senha" type="password" />
+            <div class="input-back">
+              <div class="button"><box-icon color="var(--c-white)" name="lock-alt"></box-icon></div>
+            </div>
+          </div>
+
+        </div>
+
+        <!--         <div class="user-box">
+              <div><box-icon color="var(--c-white)" name="lock-alt"></box-icon></div>
+              <input required="" v-model="user.password" placeholder="senha" type="password" />
+            </div> -->
+        <button @submit.prevent="submit" type="submit" class="btn-blue">
+          entrar
+        </button>
+      </form>
+    </div>
+
   </main>
 </template>
 
@@ -54,6 +66,7 @@ h1 {
   font-size: 1.2em;
   margin-bottom: 1em;
 }
+
 main {
   width: 100%;
   height: 77vh;
@@ -61,12 +74,21 @@ main {
   border-radius: 10px;
   overflow: hidden;
 }
+
 img {
   width: 6em;
   margin-bottom: 2em;
 }
+
 .btn-blue {
   padding: 1em;
+}
+
+.input-back {
+  border-radius: 10px;
+
+  display: flex;
+  background-color: gray;
 }
 
 .login-box {
@@ -80,18 +102,37 @@ img {
   justify-content: center;
   align-items: center;
 }
+
 .login-box .user-box {
   display: flex;
+  flex-direction: column;
 }
-.login-box .user-box div {
+
+
+.login-box .user-box .button {
   background-color: var(--c-green-500);
-  padding: 1em;
+  padding: 0.5em;
+  display: flex;
   border-radius: 10px;
-  position: absolute;
 }
+
+.input-back input:focus {
+  outline: none;
+  border: none;
+  box-shadow: 0;
+}
+
+#sem-efeito:focus~.input-back {
+  outline: none;
+  border-color: var(--c-green-500);
+  box-shadow: 0 0 0 1px var(--c-green-500), 0 0 0 4px #5aed8352;
+
+}
+
+
 input {
-  padding: 2em 5em;
-  height: auto;
-  margin-bottom: 1em;
+  background-color: transparent;
+  translate: 0vw 5vh;
+  padding-left: 4vw;
 }
 </style>
